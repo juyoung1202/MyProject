9607abe74a6953d32381953f8acd35d608e2a057
MacBook <macbook@MacBookui-MacBook-Pro.local>
2016-03-16 17:57:30 +0900

    ww

--- src/com/sd/turtle/WeightTurtle.java
+++ src/com/sd/turtle/WeightTurtle.java
@@ -3,19 +3,19 @@
  * @since 20160314
  * to demo upcasting and downcasting
  */
-
 package com.sd.turtle;
 
 import java.awt.Color;
 import ch.aplu.turtle.*;
 
-public class WeightTurtle extends Turtle{
-    private int weight;
-    public WeightTurtle(int w){
-      weight=w;
-      setPenColor(Color.red);
+public class WeightTurtle extends Turtle {
+    protected int weight;
+    public WeightTurtle(int w) {
+        weight=w;
+        setColor(Color.red);
+        setPenColor(Color.red);
     }
-    public int getWeight(){
-      return weight;
+    public int getWeight() {
+        return weight;
     }
 }
---
+++ src/headfirst/strategy/Quack.java
@@ -0,0 +1,8 @@
+package headfirst.strategy;
+
+public class Quack implrements QuackBehavior {
+  
+  public void quack() {
+    System.out.print("Quack");
+  }
+}
